4a7832af34341807bb1651da663469ec3bb8e73e
lucaboghitoi <167452784+lucaboghitoi@users.noreply.github.com>
2024-04-25 18:48:40 +0300

    Add files via upload

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -1,5 +1,31 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprites):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.Y > 5:
+            self.rect.Y -= self.speed
+        if keys[K_DOWN] and self.rect.Y < win_height - 80:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
 back = (200, 255, 255)
 win_width = 600
 win_height = 500
