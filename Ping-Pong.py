8b0aa699bcfc7ace583016611c1e006d3b6cfa71
lucaboghitoi <167452784+lucaboghitoi@users.noreply.github.com>
2024-05-09 18:43:52 +0300

    Add files via upload

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -11,12 +11,12 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player(GameSprites):
-    def update(self):
+class Player(GameSprite):
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.Y > 5:
-            self.rect.Y -= self.speed
-        if keys[K_DOWN] and self.rect.Y < win_height - 80:
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
     def update_l(self):
@@ -32,6 +32,7 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 game = True
+finish = False
 clock = time.Clock()
 FPS = 60
 
@@ -51,35 +52,34 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-     if finish != True:
-       window.fill(back)
-       racket1.update_l()
-       racket2.update_r()
-       ball.rect.x += speed_x
-       ball.rect.y += speed_y
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
-
-       if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
-           speed_x *= -1
-           speed_y *= 1
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
       
-       if ball.rect.y > win_height-50 or ball.rect.y < 0:
-           speed_y *= -1
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
 
-       if ball.rect.x < 0:
-           finish = True
-           window.blit(lose1, (200, 200))
-           game_over = True
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
 
-       if ball.rect.x > win_width:
-           finish = True
-           window.blit(lose2, (200, 200))
-           game_over = True
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
 
 
-       racket1.reset()
-       racket2.reset()
-       ball.reset()
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
 
 
     display.update()
